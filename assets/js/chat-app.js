5fe2373a82f85b75a5c3f9fd4c11398bb595d5c6
Krithik Vijayakumar <45972552+krithikV@users.noreply.github.com>
2024-04-01 08:09:20 +0530

    Update chat-app.js

--- assets/js/chat-app.js
+++ assets/js/chat-app.js
@@ -126,7 +126,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
         
-        fetch('http://127.0.0.1:5000/predict', {
+        fetch('https://krithikmyava.pythonanywhere.com/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 , session_id: session_id,city:await getCityName()}),
             mode: 'cors',
